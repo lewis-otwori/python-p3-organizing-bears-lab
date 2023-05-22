5ea96d942b616fb7cd39c0d4e456251ace89bcf9
lewis-otwori <lewisotwori@gmail.com>
2023-05-22 15:06:37 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,131 @@
+INSERT INTO bears(
+name,
+age,
+sex,
+color,
+temperament,
+alive
+
+)
+VALUES (
+"Mr. Chocolate",
+20,
+"F",
+"black",
+"sanguine",
+1
+);
+
+INSERT INTO bears(
+name,
+age,
+sex,
+color,
+temperament,
+alive
+
+)
+VALUES (
+"Mr. Chocolate",
+21,
+"F",
+"black",
+"sanguine",
+1
+);
+
+INSERT INTO bears(
+name,
+age,
+sex,
+color,
+temperament,
+alive
+
+)
+VALUES (
+"Mr. Chocolate",
+26,
+"F",
+"black",
+"sanguine",
+1
+);
+
+INSERT INTO bears(
+name,
+age,
+sex,
+color,
+temperament,
+alive
+
+)
+VALUES (
+"Mr. Chocolate",
+30,
+"F",
+"black",
+"sanguine",
+1
+);
+
+INSERT INTO bears(
+name,
+age,
+sex,
+color,
+temperament,
+alive
+
+)
+VALUES (
+"Mr. Chocolate",
+25,
+"F",
+"black",
+"sanguine",
+1
+);
+
+INSERT INTO bears(
+name,
+age,
+sex,
+color,
+temperament,
+alive
+
+)
+VALUES (
+"Mr. Chocolate",
+20,
+"F",
+"black",
+"sanguine",
+1
+);
+
+INSERT INTO bears(
+name,
+age,
+sex,
+color,
+temperament,
+alive
+
+)
+VALUES (
+"Mr. Chocolate",
+20,
+"F",
+"black",
+"sanguine",
+1
+);
+
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 12, "M", "brown", "melangoli", 1);
+
+
